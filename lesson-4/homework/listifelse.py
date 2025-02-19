8dc63cc0428abf5ea6c586f507f2ae6a01ca0a61
Miramirodilova <mohiramirodilova6@gmail.com>
2025-02-19 14:26:12 +0500

    Create listifelse.py

---
+++ lesson-4/homework/listifelse.py
@@ -0,0 +1,16 @@
+food =["rice", "beans"]
+food.append("Broccoli")
+print (food)
+food.extend(["bread", "pizza"])
+print (food)
+print(food[:2])
+print(food[-1])
+breakfast ="eggs, fruit, orange juice".split(", ")
+print (breakfast)
+print(len(breakfast))
+lengths = [len(item) for item in breakfast]
+print (lengths)
+
+
+
+// + if else task from website
